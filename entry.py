a033e6e51fe736614317b7a535ed582cbb107596
Muspi Merol <wjj123345567@icloud.com>
2024-05-29 07:36:52 +0000

    fix: force to `spawn` subprocesses

--- entry.py
+++ entry.py
@@ -1,5 +1,8 @@
 if __name__ == "__main__":
+    from multiprocessing import set_start_method
+
     from src import distributions, languages
 
+    set_start_method("spawn", True)
     languages.main()
     distributions.main()
