34417a2d2b94595d6d325d0f386dac44e39f8f43
Muspi Merol <wjj123345567@icloud.com>
2024-06-04 00:13:00 +0800

    feat: improve error handling and skip some bad strings

--- src/core/moderate.py
+++ src/core/moderate.py
@@ -1,5 +1,6 @@
 from asyncio import Semaphore, ensure_future, gather
 from functools import cache
+from itertools import chain
 from pathlib import Path
 from typing import Callable, Iterable
 
@@ -41,15 +42,29 @@ class ModerationPipeline:
             for line_index, moderation in items:
                 await file.write(encode((line_index, moderation)))
 
-    async def moderate(self, strings: list[str]) -> list[Moderation]:
+    async def moderate(self, inputs: list[tuple[int, str]]) -> Iterable[tuple[int, Moderation]]:
+        if not inputs:
+            return []
+
         from openai import APIError
 
+        retry_count = 0
         while True:
             try:
+                indices, strings = zip(*inputs)
                 res = await get_client().moderations.create(input=strings, extra_headers={"Authorization": f"Bearer {get_api_key()}"} if key_count > 1 else None)
-                return res.model_dump(include={"results"})["results"]
+                return zip(indices, res.model_dump(include={"results"})["results"])
             except APIError as e:
+                retry_count += 1
                 print(e.type, e.message)
+                if retry_count > 3:
+                    if len(inputs) == 1:
+                        if retry_count > 10:
+                            return ()
+                        print(inputs[0][1])
+                    else:
+                        index = len(inputs) // 2
+                        return chain(await self.moderate(inputs[:index]), await self.moderate(inputs[index:]))
 
     async def process_file(self, file: File, callback: Callable | None = None):
         if (root / file.path.name).with_suffix(".msgpack").exists():
@@ -66,8 +81,8 @@ class ModerationPipeline:
                 if not strings:
                     return
 
-                results = await self.moderate(strings)
-                items.extend(zip(indices, results))
+                results = await self.moderate(list(zip(indices, strings)))
+                items.extend(results)
 
             line_index = 0
 
