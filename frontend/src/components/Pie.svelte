e886f70e1b5e3e4aa2d379214ffd703519d69bb2
Muspi Merol <wjj123345567@icloud.com>
2024-06-05 07:55:03 +0800

    feat: add a pie plot

---
+++ frontend/src/components/Pie.svelte
@@ -0,0 +1,71 @@
+<script lang="ts">
+  import * as echarts from "echarts"
+  import { onMount } from "svelte"
+
+  export let title: string
+  export let data: Record<string, number>
+
+  let div: HTMLDivElement
+
+  let width: number
+  let height: number
+
+  let chart: echarts.ECharts
+
+  function resize(width: number, height: number) {
+    if (chart) {
+      chart.resize({ width: width || "auto", height: height || "auto" })
+    }
+  }
+
+  let length = Object.keys(data)
+    .map(Number)
+    .sort((a, b) => a - b)
+    .at(-1)!
+
+  length = Math.min(length, 20000)
+
+  const xs = Array.from({ length }).map((_, i) => i)
+  const ys = Array.from({ length }).map((_, i) => data[String(i)] ?? 0)
+
+  $: resize(width, height)
+
+  onMount(() => {
+    chart = echarts.init(div, null, { width, height })
+
+    chart.setOption({
+      tooltip: {
+        trigger: "item",
+      },
+      series: [
+        {
+          type: "pie",
+          radius: ["30%", "70%"],
+          avoidLabelOverlap: false,
+          padAngle: 4,
+          itemStyle: {
+            borderRadius: 10,
+          },
+          emphasis: {
+            label: {
+              show: true,
+              fontSize: 40,
+            },
+          },
+          data: Object.entries(data).map(([key, value]) => ({ value, name: key })),
+        },
+      ],
+    } as echarts.EChartsOption)
+  })
+</script>
+
+<svelte:window
+  on:resize={() => {
+    width = div.clientWidth
+    height = div.clientHeight
+  }}
+/>
+
+<div class="m-20 h-3/4 w-3/4">
+  <div class="h-full w-full" bind:this={div} bind:clientWidth={width} bind:clientHeight={height} />
+</div>
---
+++ frontend/src/routes/ppt/4/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+  import Pie from "$components/Pie.svelte"
+  import { ARABIC, CHINESE, ENGLISH, JAPANESE, RUSSIAN } from "$output/languages.json"
+</script>
+
+<Pie title="语言" data={{ ARABIC, CHINESE, ENGLISH, JAPANESE, RUSSIAN }} />
