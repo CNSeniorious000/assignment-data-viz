e0cf4b16d6d41615cd82ae7c5ffa0a80ba00e160
Muspi Merol <wjj123345567@icloud.com>
2024-06-05 07:57:37 +0800

    feat: show moderation results in pie charts

---
+++ frontend/src/routes/ppt/5/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+  import Pie from "$components/Pie.svelte"
+  import { count } from "$output/flags.json"
+</script>
+
+<Pie title="合规性" data={{ unflagged: count[0], flagged: count[1] }} />
---
+++ frontend/src/routes/ppt/6/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+  import Pie from "$components/Pie.svelte"
+  import { flags as data } from "$output/flags.json"
+</script>
+
+<Pie title="违规分类" {data} />
