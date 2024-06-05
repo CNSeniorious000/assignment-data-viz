6f35696982e85b904b24123a3d11ce087a6f7885
Muspi Merol <wjj123345567@icloud.com>
2024-06-05 08:48:59 +0800

    fix: downgrade `jiti` and fix linting errors

--- frontend/src/routes/ppt/3/+page.svelte
+++ frontend/src/routes/ppt/3/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-  import Bar from "$components/Bar.svelte";
-  import { conversation_length } from "$output/distributions.json";
+  import Bar from "$components/Bar.svelte"
+  import { conversation_length } from "$output/distributions.json"
 </script>
 
 <Bar title="对话消息条数" data={conversation_length} />
